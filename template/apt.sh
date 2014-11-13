b2fff6b10d7904a8dfa2cd1989ce01d415570e5b
Manfred Touron <m@42.am>
2014-11-13 12:47:19 +0100

    Removed utils-ping

--- template/apt.sh
+++ template/apt.sh
@@ -1,3 +1,3 @@
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install linux-headers-$(uname -r) build-essential emacs24-nox htop tmux utils-ping less net-tools ssh wget socat lsof tcpdump netcat ioping fio locate ethstatus dstat file iotop
+apt-get -y install linux-headers-$(uname -r) build-essential emacs24-nox htop tmux less net-tools ssh wget socat lsof tcpdump netcat ioping fio locate ethstatus dstat file iotop
